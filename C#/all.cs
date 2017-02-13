e3c929bee5a8fb0acce9f6396eb8976ab220148d
amytnyk <alex.mytnyk@ukr.net>
2017-02-14 01:06:10 +1000

    Added namespace, factorial
    init

--- C#/all.cs
+++ C#/all.cs
@@ -1,13 +1,41 @@
 namespace ML
 {
-    int gcd(int a, int b)
+    public class Fact()
     {
-        while (b != 0)
+        public int value;
+        public Fact(int N)
         {
-            int r = a % b;
-            a = b;
-            b = r;
+            if(N < 0)
+                return 0;
+            if (N == 0)
+                return 1;
+            else
+                return N * Fact(N - 1);
         }
-        return a;
+    }
+    
+    public class Gcd
+    {
+        public int value;
+        public Gcd(int a, int b)
+        {
+            while (b != 0)
+            {
+                int r = a % b;
+                a = b;
+                b = r;
+            }
+         this.value = a;
+        }
+    }
+    
+    public class Lcm
+    {
+      public int value;
+      public Lcm(int a, int b)
+      {
+        Gcd gcd = new Gcd(a, b);
+        this.value = a /gcd.value * b;
+      }
     }
 }
